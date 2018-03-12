015c6c59882e4a56bd5ded6336346a760820815f
Darshan Pradhan <darshan.pradhan@gmail.com>
2018-03-12 00:26:14 -0400

    updates

--- beats/filebeats/DockerRun.sh
+++ beats/filebeats/DockerRun.sh
@@ -1,4 +1,5 @@
-docker run -it \
-    -v  /Users/daddy/WebstormProjects/elk-docker/elk/logstash/logstash-beats.crt:/etc/pki/tls/certs/logstash-beats.crt \
-    -v
-    docker.elastic.co/beats/filebeat:6.2.2
+docker run -it -d \
+    -v /Users/daddy/WebstormProjects/elk-docker/elk/logstash/logstash-beats.crt:/etc/pki/tls/certs/logstash-beats.crt \
+    -v /Users/daddy/WebstormProjects/elk-docker/beats/filebeats/filebeat-logstash-poc.yml:/usr/share/filebeat/filebeat.yml \
+    -v /Users/daddy/WebstormProjects/elk-docker/beats/filebeats/data/globalTimings.csv:/var/log/custom/globalTimings.csv \
+    --name filebeat docker.elastic.co/beats/filebeat:6.2.2
