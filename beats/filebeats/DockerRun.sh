1c6765cc05a51e0d18c2ccc2f7cc0dfee68a4134
Darshan Pradhan <darshan.pradhan@gmail.com>
2018-03-11 23:24:26 -0400

    added files

---
+++ beats/filebeats/DockerRun.sh
@@ -0,0 +1,4 @@
+docker run -it \
+    -v  /Users/daddy/WebstormProjects/elk-docker/elk/logstash/logstash-beats.crt:/etc/pki/tls/certs/logstash-beats.crt \
+    -v
+    docker.elastic.co/beats/filebeat:6.2.2
